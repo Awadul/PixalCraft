d35415843819bc4a6d3ff3ac7b8003e06fee3482
Muhammad Awais Abdullah <143254491+Awadul@users.noreply.github.com>
2025-08-23 21:38:42 +0500

    changed the home page

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,10 +1,10 @@
 import React from "react";
-import Demos from "./demos/demos";
+import Homepage5 from "./homepage/home5-dark";
 
 const Home = () => {
   return (
     <>
-      <Demos />
+      <Homepage5 />
     </>
   );
 };
